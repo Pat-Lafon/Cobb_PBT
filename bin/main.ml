b6bfdb37040b2787d8e5f5f56dca081ce0d0d807
Patrick LaFontaine <32135464+Pat-Lafon@users.noreply.github.com>
2024-12-10 21:01:40 -0500

    Rename some parts with a comment

--- bin/main.ml
+++ bin/main.ml
@@ -1,6 +1,7 @@
+(* This is a dummy user functon that always works *)
 let func _l = true[@@ gen]
 
-let sized_list n l = List.length l <= n
+let is_sized n l = List.length l <= n
 
 (* let is_sorted l = failwith "TODO" *)
 
@@ -8,17 +9,17 @@ let sized_list n l = List.length l <= n
 
 (* let is_unique l = failwith "TODO" *)
 
-let bad_test =
+let precondition_frequency =
   QCheck.(Test.make
   ~count:20000
   ~name:"tests_R_us"
-   (pair (int)  (list int)) (fun (n, l) ->
-      assume (sized_list n l);
+   (pair (int) (list int)) (fun (n, l) ->
+      assume (is_sized n l);
       func l))
 
-let () = QCheck_runner.run_tests_main ~argv:[|"-v"; "--verbose"; "--seed"; "0"|] [bad_test]
+let () = QCheck_runner.run_tests_main ~argv:[|"-v"; "--verbose"; "--seed"; "0"|] [precondition_frequency]
 
-(* let bad_test =
+(* let precondition_frequency =
   QCheck.(Test.make
   ~count:20000
   ~name:"tests_R_us"
@@ -26,15 +27,5 @@ let () = QCheck_runner.run_tests_main ~argv:[|"-v"; "--verbose"; "--seed"; "0"|]
       assume (is_sorted l);
       func l))
 
-let () = QCheck_runner.run_tests_main ~argv:[|"-v"; "--verbose"; "--seed"; "0"|] [bad_test]
+let () = QCheck_runner.run_tests_main ~argv:[|"-v"; "--verbose"; "--seed"; "0"|] [precondition_frequency]
  *)
-
-(* let bad_assume_warn =
-  QCheck.Test.make ~count:2_000
-    ~name:"WARN_unlikely_precond"
-    QCheck.int
-    (fun x ->
-       QCheck.assume (x mod 100 = 1);
-       true)
-
-let () = QCheck_runner.run_tests_main [bad_assume_warn] *)
