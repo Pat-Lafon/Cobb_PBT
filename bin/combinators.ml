db731f92268d72536ff75fab3f78713baafaf599
Francille <129557858+Francille@users.noreply.github.com>
2025-02-12 00:17:53 -0500

    Changed signature of my int_list, added -n output filename for output.py

--- bin/combinators.ml
+++ bin/combinators.ml
@@ -1,10 +1,14 @@
 Random.init 2025
 
-let rec int_list s = 
+let rec int_list_size_gen s st = 
   if s <= 0 then
     []
   else
-    Random.int 100000 :: int_list (s - 1)
+    QCheck.Gen.int st :: int_list_size_gen (s - 1) st
+
+let int_list_gen st = int_list_size_gen (QCheck.Gen.nat st) st
+
+let int_list = QCheck.make (int_list_gen)
 
 let rec print_list l = match l with 
 | [] -> print_newline ()
