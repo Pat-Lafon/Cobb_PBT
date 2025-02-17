e535a77da271bd80b17dfe2b73e4c77ba221949e
Francille <129557858+Francille@users.noreply.github.com>
2025-02-17 00:22:42 -0500

    Abstracted away random state st
    Not 100% sure this is right, I'll ask for clarification in the morning. I'm using a ref, but not with option (I'm not sure if that's significant)

--- bin/combinators.ml
+++ bin/combinators.ml
@@ -3,50 +3,57 @@
   - shortcut for the function signature 
   I'll probably create my own version tho *)
 
-let rec int_list_size_gen s st = 
+(* trying to abstract away random state, not sure what
+intentions are, will ask for clarification on Monday *)
+let st = ref (Random.State.make [| 2100 |])
+
+(* removed st parameter *)
+let rec int_list_size_gen s = 
   if s <= 0 then
     []
   else
-    QCheck.Gen.int st :: int_list_size_gen (s - 1) st
+    (* using st initialized in ref *)
+    QCheck.Gen.int !st :: int_list_size_gen (s - 1)
 
 (* int list sorted in ascending order *)
-let rec int_list_sorted_gen prev s st = 
+let rec int_list_sorted_gen prev s = 
   if s <= 0 then
     []
   else
-    let n = QCheck.Gen.int st in
+    let n = QCheck.Gen.int !st in
     if n >= prev then
-      n :: int_list_sorted_gen n (s - 1)st
+      n :: int_list_sorted_gen n (s - 1)
     else 
-      int_list_sorted_gen prev (s - 1) st
+      int_list_sorted_gen prev (s - 1)
 
 (* int list with each element identical *)
-let int_list_dup_gen s st = 
-  let n = QCheck.Gen.int st in
-  let rec aux s' st' =
+let int_list_dup_gen s = 
+  let n = QCheck.Gen.int !st in
+  let rec aux s' =
     if s' <= 0 then
       []
     else
-        n :: aux (s' - 1) st' in
-  aux s st
+        n :: aux (s' - 1) in
+  aux s 
 
 (* int list with each element unique *)
 (* ignore, still in progress *)
-let int_list_unique_gen s st = 
+let int_list_unique_gen s = 
   let set = Hashtbl.create s in
-  let rec aux s' st' len =
+  let rec aux s' len =
     if s' <= 0 then
       []
     else
-      let n = QCheck.Gen.int st' in
+      let n = QCheck.Gen.int !st in
       Hashtbl.replace set n ();
       if len <> Hashtbl.length set then 
-        n :: aux (s' - 1) st' (len + 1) else 
-        aux (s' - 1) st' len in
-  aux s st 1
+        n :: aux (s' - 1) (len + 1) else 
+        aux (s' - 1) len in
+  aux s 1
 
 (* higher order programming *)
-let list f = QCheck.make (fun st -> f (QCheck.Gen.nat st) st)
+(* make still expects random state as parameter, "_" gets rid of it*)
+let list f = QCheck.make (fun _ -> f (QCheck.Gen.nat !st) )
 
 let int_list = list int_list_size_gen
 let int_list_sorted = list (int_list_sorted_gen 0)
