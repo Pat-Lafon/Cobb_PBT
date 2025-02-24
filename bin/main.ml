ac394ffa24edf26b53ad2649939fab4369d891f7
Francille <129557858+Francille@users.noreply.github.com>
2025-02-24 08:03:03 -0500

    Question about combinators signatures

--- bin/main.ml
+++ bin/main.ml
@@ -24,22 +24,30 @@ let precondition_frequency prop name gen_type =
   QCheck.(Test.make
   ~count:20000
   ~name
-   (gen_type) prop)
+    (pair (int) (gen_type)) (fun (n, l) ->
+      assume (prop n l);
+      func l))
 
-let assume_prop prop l = QCheck.assume (prop l); func l
+(* let assume_prop prop l = QCheck.assume (prop l); func l *)
+
+let precondition_frequency_size = precondition_frequency is_sized "is_sized" default_gen
+let precondition_frequency_sort = precondition_frequency (fun _ l -> is_sorted l) "is_sorted" default_gen
+let precondition_frequency_dup = precondition_frequency (fun _ l -> is_duplicate l) "is_duplicate" default_gen
+let precondition_frequency_unique = precondition_frequency (fun _ l -> is_unique l) "is_unique" default_gen
 
 (* should QCheck.int be replaced with our own gen that has st abstracted away? *)
 (* there are also considerations with running the precondition for size, and how it also needs to accesss the size *)
-let precondition_frequency_size = precondition_frequency ((fun prop (n, l) -> QCheck.assume (prop n l); func l ) is_sized) 
+(* let precondition_frequency_size = precondition_frequency ((fun prop (n, l) -> QCheck.assume (prop n l); func l ) is_sized) 
   "is_sized" (QCheck.pair (QCheck.int) (default_gen))
 (* let precondition_frequency_size = precondition_frequency (assume_prop_pair is_sized) "is_sized" (QCheck.pair (QCheck.int) (Combinators.int_list_size)) *)
 let precondition_frequency_sort = precondition_frequency (assume_prop is_sorted) "is_sorted" default_gen
 let precondition_frequency_dup = precondition_frequency (assume_prop is_duplicate) "is_duplicate" default_gen
-let precondition_frequency_unique = precondition_frequency (assume_prop is_unique) "is_unique" default_gen
+let precondition_frequency_unique = precondition_frequency (assume_prop is_unique) "is_unique" default_gen *)
 
 
 (* list of all possible tests to run *)
 let sample_tests = [precondition_frequency_size; precondition_frequency_sort; precondition_frequency_dup; precondition_frequency_unique]
+
 
 (* command line args *)
 let args = Array.to_list(Sys.argv)
@@ -47,9 +55,8 @@ let args = Array.to_list(Sys.argv)
 (* returns name of the test *)
 let t_get_name (QCheck2.Test.Test c) = QCheck2.Test.get_name c
 
-(* condition for filter *)
-let is_required (test : QCheck2.Test.t) = List.mem (t_get_name test) args 
-
+let is_required (test : QCheck2.Test.t) = List.mem (t_get_name test) args
+  
 let () = 
   let argc = Array.length Sys.argv in
   let (tests) = 
@@ -80,4 +87,56 @@ let () =
       in ()
 
     else 
-      let () = QCheck_runner.run_tests_main ~argv:[|"-v"; "--verbose"; "--seed"; "0"|] tests in ()
+      let () = QCheck_runner.run_tests_main ~argv:[|"-v"; "--verbose"; "--seed"; "0"|] tests in () 
+
+(* tried using Arg.parse for better cli, 
+  however, Arg.parse is probably interfering with QCheck_runner parameters *)
+      
+(*
+(* returns name of the test *)
+let t_get_name (QCheck2.Test.Test c) = QCheck2.Test.get_name c
+
+let usage_msg = "dune exec Cobb_PBT [-o] [-g] <generator> [-t] <test1> [<test2>] ... "
+
+let output_to_file = ref false
+let test_names = ref []
+(* let generator = ref "" *)
+
+let anon_fun testname = test_names := testname :: !test_names
+
+let speclist =
+  [
+    ("-o", Arg.Set output_to_file, "Output test results to txt file");
+    (* ("-g", Arg.Set_string generator, "Set generator type"); *)
+    ("-t", Arg.String anon_fun, "Set tests")
+  ]
+
+let () = 
+  Arg.parse speclist anon_fun usage_msg;
+  (* Printf.printf "Flag -o: %b\n" !output_to_file;
+  Printf.printf "Flag -g: %s\n" !generator;
+  Printf.printf "Extra arguments: [%s]\n" (String.concat ", " !test_names); *)
+
+  let tests = 
+    if List.is_empty !test_names then
+      sample_tests
+    else
+    (* condition for filter *)
+    let is_required (test : QCheck2.Test.t) = List.mem (t_get_name test) !test_names in
+    List.filter is_required sample_tests 
+  in 
+  if !output_to_file then
+    let oc = open_out file in
+      let fmt = Format.formatter_of_out_channel oc in
+      Format.fprintf fmt "Running QCheck Tests with options: %a\n\n" 
+        (Format.pp_print_list Format.pp_print_string) (!test_names);
+
+      (* Run the tests using QCheck_runner.run_tests *)
+      let _ = QCheck_runner.run_tests ~verbose:true ~out:oc tests in
+      close_out oc
+  else begin
+    let st = Random.State.make [|0|] in
+    let _ = QCheck_runner.run_tests ~verbose:true ~rand:st tests in ()
+    (* QCheck_runner.run_tests_main ~argv:[|"-v"; "--verbose"; "--seed"; "0"|] tests; *)
+  end
+*)
