fce40b6bd3df521716430b4f7b1426aa218d4d5c
Francille <129557858+Francille@users.noreply.github.com>
2025-03-02 21:19:30 -0500

    Fixed unique_list_progX.safe, added duplicate_list_progX.syn/cov/safe

---
+++ bin/unique_list/prog2_safe.ml
@@ -0,0 +1,7 @@
+open Combinators
+let rec unique_list_gen (s : int) : int list =
+  if sizecheck s then []
+  else
+    let (l : int list) = unique_list_gen (subs s) in
+    let (x : int) = int_gen () in
+    if list_mem l x then raise BailOut else x :: l
