b1fa8dd2617f7d0f5c197302f2b3bc972c73128a
Francille <129557858+Francille@users.noreply.github.com>
2025-03-05 02:18:38 -0500

    created the second table

--- bin/main.ml
+++ bin/main.ml
@@ -1,10 +1,11 @@
 let file = "results.txt"
 let test_count = 20000
 let test_max_fail = 20000
- 
+
 let func _l = true[@@ gen]
 
-(* list preconditions *)
+
+(* preconditions for list *)
 let is_sized n l = List.length l <= n
 
 let rec is_sorted = function
@@ -15,14 +16,37 @@ let rec is_duplicate = function
 | h1::h2::t -> if h1 = h2 then is_duplicate (h2::t) else false
 | _ -> true
 
-let is_unique l = 
+let is_unique l =
   let len = List.length l in
   let set = Hashtbl.create len in
-  let () = List.iter (fun x -> Hashtbl.replace set x ()) l in 
+  let () = List.iter (fun x -> Hashtbl.replace set x ()) l in
   len = Hashtbl.length set
 
+
+(* do the filter functions need to check for size? *)
+(* safety filter functions for duplicate *)
+let is_duplicate_prog1_safe l = is_duplicate l
+let is_duplicate_prog2_safe l = is_duplicate l
+(* the safety repairs are just the correct repairs *)
+
+(* safety filter functions for unique *)
+let is_unique_prog1_safe l = is_unique l
+let is_unique_prog2_safe l = is_unique l
+let is_unique_prog3_safe l = is_unique l
+
+(* safety filter functions for sized *)
+let is_sized_prog1_safe n l = is_sized n l
+let is_sized_prog2_safe n l = is_sized n l
+let is_sized_prog3_safe _ l = l = []
+
+(* safety filter functions for sorted *)
+let is_sorted_prog1_safe l = is_duplicate l
+let is_sorted_prog2_safe l = is_duplicate l
+
+
+
 (* QCheck.make *)
-let precondition_frequency prop name gen_type =
+let precondition_frequency prop (gen_type, name) =
   QCheck.(Test.make
   ~count:test_count
   ~max_fail:test_max_fail
@@ -31,7 +55,7 @@ let precondition_frequency prop name gen_type =
       assume (prop l);
       func l))
 
-let precondition_frequency_pair prop name gen_type =
+let precondition_frequency_pair prop (gen_type, name) =
   QCheck.(Test.make
   ~count:test_count
   ~max_fail:test_max_fail
@@ -41,28 +65,16 @@ let precondition_frequency_pair prop name gen_type =
       func l))
 
 
-(* let precondition_frequency_size' (gen, name) = 
-  let state = Random.State.make [|0|] in
-  let size = (*100*) Arbitrary_builder.int.gen state in 
-  precondition_frequency (is_sized size) name (gen size) *)
 
-let precondition_frequency_size (gen, name) = precondition_frequency_pair is_sized name gen
-let precondition_frequency_sort (gen, name) = precondition_frequency is_sorted name gen
-let precondition_frequency_dup (gen, name) = precondition_frequency is_duplicate name gen
-let precondition_frequency_unique (gen, name) = precondition_frequency is_unique name gen
-
-(* creates tests for each generator *)
-let sized_list_tests = List.map precondition_frequency_size Arbitrary_builder.sized_list_arbitraries
-let duplicate_list_tests = List.map precondition_frequency_dup Arbitrary_builder.duplicate_list_arbitraries
-let unique_list_tests = List.map precondition_frequency_unique Arbitrary_builder.unique_list_arbitraries
-let sorted_list_tests = List.map precondition_frequency_sort Arbitrary_builder.sorted_list_arbitraries
+let create_test_list prop gen_name = List.map (precondition_frequency prop) gen_name
+let create_test_pair_list prop gen_name = List.map (precondition_frequency_pair prop) gen_name
 
 (* tests: gets run by qcheck *)
 (* maybe I should make a funtion for running tests to make it clear *)
-let tests = sized_list_tests
+let tests = [precondition_frequency_pair is_sized_prog3_safe (Arbitrary_builder.example, "prop3_safe")]
 
 (* foldername: where results gets outputted to if -o *)
-let foldername = "./bin/sorted_list/"
+let foldername = "./bin/sized_list/safe_prop/"
 
 
 (* command line args *)
@@ -72,18 +84,18 @@ let args = Array.to_list(Sys.argv)
 let t_get_name (QCheck2.Test.Test c) = QCheck2.Test.get_name c
 
 let is_required (test : QCheck2.Test.t) = List.mem (t_get_name test) args
-  
-let () = 
+
+let () =
   let argc = Array.length Sys.argv in
-  (* let tests = 
+  (* let tests =
     if Array.mem "-t" Sys.argv then
       if argc >= 3 then
         (* filters out non-name tests *)
         List.filter is_required tests
-      else 
+      else
         let () = print_endline "usage: <program> -t -o <test1> <test2> ..." in
         []
-    else 
+    else
       tests
     in *)
 
@@ -91,7 +103,7 @@ let () =
       if Array.mem "-o" Sys.argv then
         let () =
           (* runs each test with see 0 *)
-          let _ = List.iter (fun g -> 
+          let _ = List.iter (fun g ->
             QCheck_runner.set_seed 0;
             let filename = foldername ^ (t_get_name g) ^ ".result" in
             let oc = open_out filename in
@@ -99,20 +111,20 @@ let () =
             close_out oc
             ) tests in ()
         in ()
-      else 
+      else
         (* runs each test with see 0 *)
-        let () = List.iter (fun g -> 
+        let () = List.iter (fun g ->
           QCheck_runner.set_seed 0;
           ignore (QCheck_runner.run_tests ~verbose:true [g]);
-          ) tests 
+          ) tests
         in ()
 
 
 
 
-(* tried using Arg.parse for better cli, 
+(* tried using Arg.parse for better cli,
   however, Arg.parse is probably interfering with QCheck_runner parameters *)
-      
+
 (*
 (* returns name of the test *)
 let t_get_name (QCheck2.Test.Test c) = QCheck2.Test.get_name c
@@ -132,24 +144,24 @@ let speclist =
     ("-t", Arg.String anon_fun, "Set tests")
   ]
 
-let () = 
+let () =
   Arg.parse speclist anon_fun usage_msg;
   (* Printf.printf "Flag -o: %b\n" !output_to_file;
   Printf.printf "Flag -g: %s\n" !generator;
   Printf.printf "Extra arguments: [%s]\n" (String.concat ", " !test_names); *)
 
-  let tests = 
+  let tests =
     if List.is_empty !test_names then
       sample_tests
     else
     (* condition for filter *)
     let is_required (test : QCheck2.Test.t) = List.mem (t_get_name test) !test_names in
-    List.filter is_required sample_tests 
-  in 
+    List.filter is_required sample_tests
+  in
   if !output_to_file then
     let oc = open_out file in
       let fmt = Format.formatter_of_out_channel oc in
-      Format.fprintf fmt "Running QCheck Tests with options: %a\n\n" 
+      Format.fprintf fmt "Running QCheck Tests with options: %a\n\n"
         (Format.pp_print_list Format.pp_print_string) (!test_names);
 
       (* Run the tests using QCheck_runner.run_tests *)
--- bin/sorted_list/prog2_safe.ml
+++ bin/sorted_list/prog2_safe.ml
@@ -3,5 +3,5 @@ let rec sorted_list_gen (s : int) (x : int) : int list =
   if sizecheck s then []
   else
     let (y : int) = int_gen () in
-    if x <= y then y :: sorted_list_gen (subs s) y else raise BailOut
+    if x <= y then x :: sorted_list_gen (subs s) x else raise BailOut
 
