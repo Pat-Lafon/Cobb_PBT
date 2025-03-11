92543c0f13505223233c8d57adf7ef34a45cd000
Patrick LaFontaine <32135464+Pat-Lafon@users.noreply.github.com>
2025-03-11 16:09:35 -0400

    rbtree getting data and results

--- bin/rbtree/prog3_safe.ml
+++ bin/rbtree/prog3_safe.ml
@@ -1,3 +1,5 @@
+open Combinators
+
 let rec rbtree_gen (inv : int) (color : bool) (h : int) : int rbtree =
   if sizecheck h then
     if color then Rbtleaf else if bool_gen () then Rbtleaf else Rbtleaf
@@ -15,18 +17,3 @@ let rec rbtree_gen (inv : int) (color : bool) (h : int) : int rbtree =
       let (lt4 : int rbtree) = rbtree_gen (subs (subs inv)) false (subs h) in
       let (rt4 : int rbtree) = rbtree_gen (subs (subs inv)) false (subs h) in
       Rbtnode (false, lt4, int_gen (), rt4)
-
-let[@assert] rbtree_gen =
-  let inv = (v >= 0 : [%v: int]) [@over] in
-  let color = (true : [%v: bool]) [@over] in
-  let[@assert] h =
-    (v >= 0 && if color then v + v == inv else v + v + 1 == inv
-      : [%v: int])
-      [@over]
-  in
-  (num_black v h && no_red_red v
-   &&
-   if color then not (rb_root_color v true)
-   else (h == 0) #==> (not (rb_root_color v false))
-    : [%v: int rbtree])
-    [@under]
