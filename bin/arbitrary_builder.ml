2c9fa3e306f0cdf9d5ebadfc844178c348a812d5
Patrick LaFontaine <32135464+Pat-Lafon@users.noreply.github.com>
2025-03-11 00:33:14 -0400

    Fix prog7 to be prog7

--- bin/arbitrary_builder.ml
+++ bin/arbitrary_builder.ml
@@ -13,12 +13,12 @@ let int_list_dup = arb_builder int_list_dup_gen
 let int_list_unique = arb_builder int_list_unique_gen
 
 (* input wrappers for Cobb generators *)
-let pair_size f () = 
+let pair_size f () =
   let size = nat_gen () in
   (size, f size)
 
 let size_wrapper f () =
-  let x = nat_gen () in 
+  let x = nat_gen () in
   f x
 
 let size_int_wrapper f () =
@@ -35,31 +35,31 @@ let catch_err f (s : int) (x : int) (*: 'a option list*) =
 (* Cobb synthesized generators *)
 
 (* sized lists *)
-let sized_list_generators = 
-  [ (Sized_list.Prog.sized_list_gen, "prog") ; 
+let sized_list_generators =
+  [ (Sized_list.Prog.sized_list_gen, "prog") ;
   (Sized_list.Prog1_syn.sized_list_gen, "prog1_syn") ; (Sized_list.Prog2_syn.sized_list_gen, "prog2_syn") ; (Sized_list.Prog3_syn.sized_list_gen, "prog3_syn") ;
   (Sized_list.Prog4_syn.sized_list_gen, "prog4_syn") ; (Sized_list.Prog5_syn.sized_list_gen, "prog5_syn") ; (Sized_list.Prog6_syn.sized_list_gen, "prog6_syn") ;
-  (Sized_list.Prog8_syn.sized_list_gen, "prog7_syn") ; (Sized_list.Prog8_syn.sized_list_gen, "prog8_syn") ; (Sized_list.Prog9_syn.sized_list_gen, "prog9_syn") ;
-  (Sized_list.Prog1_cov.sized_list_gen, "prog1_cov") ; (Sized_list.Prog2_cov.sized_list_gen , "prog2_cov")  ; (Sized_list.Prog3_cov.sized_list_gen , "prog3_cov") ; 
-  (Sized_list.Prog4_cov.sized_list_gen, "prog4_cov") ; (Sized_list.Prog5_cov.sized_list_gen , "prog5_cov")  ; (Sized_list.Prog6_cov.sized_list_gen , "prog6_cov") ; 
-  (Sized_list.Prog7_cov.sized_list_gen, "prog7_cov") ; (Sized_list.Prog8_cov.sized_list_gen , "prog8_cov")  ; (Sized_list.Prog9_cov.sized_list_gen , "prog9_cov") ; 
+  (Sized_list.Prog7_syn.sized_list_gen, "prog7_syn") ; (Sized_list.Prog8_syn.sized_list_gen, "prog8_syn") ; (Sized_list.Prog9_syn.sized_list_gen, "prog9_syn") ;
+  (Sized_list.Prog1_cov.sized_list_gen, "prog1_cov") ; (Sized_list.Prog2_cov.sized_list_gen , "prog2_cov")  ; (Sized_list.Prog3_cov.sized_list_gen , "prog3_cov") ;
+  (Sized_list.Prog4_cov.sized_list_gen, "prog4_cov") ; (Sized_list.Prog5_cov.sized_list_gen , "prog5_cov")  ; (Sized_list.Prog6_cov.sized_list_gen , "prog6_cov") ;
+  (Sized_list.Prog7_cov.sized_list_gen, "prog7_cov") ; (Sized_list.Prog8_cov.sized_list_gen , "prog8_cov")  ; (Sized_list.Prog9_cov.sized_list_gen , "prog9_cov") ;
   (Sized_list.Prog1_safe.sized_list_gen , "prog1_syn")  ; (Sized_list.Prog2_safe.sized_list_gen , "prog2_syn") ; (Sized_list.Prog3_safe.sized_list_gen , "prog3_syn") ;
   (Sized_list.Prog4_safe.sized_list_gen , "prog4_syn")  ; (Sized_list.Prog5_safe.sized_list_gen , "prog5_syn") ; (Sized_list.Prog6_safe.sized_list_gen , "prog6_syn") ;
   (Sized_list.Prog7_safe.sized_list_gen , "prog7_syn")  ; (Sized_list.Prog8_safe.sized_list_gen , "prog8_syn") ; (Sized_list.Prog9_safe.sized_list_gen , "prog9_syn") ;
   ]
 
 (* duplicate lists *)
-let duplicate_list_generators = 
+let duplicate_list_generators =
   [ (Duplicate_list.Prog.duplicate_list_gen, "prog") ;
-  (Duplicate_list.Prog1_syn.duplicate_list_gen, "prog1_syn") ; (Duplicate_list.Prog2_syn.duplicate_list_gen, "prog2_syn") ; (Duplicate_list.Prog3_syn.duplicate_list_gen, "prog3_syn") ; 
+  (Duplicate_list.Prog1_syn.duplicate_list_gen, "prog1_syn") ; (Duplicate_list.Prog2_syn.duplicate_list_gen, "prog2_syn") ; (Duplicate_list.Prog3_syn.duplicate_list_gen, "prog3_syn") ;
   (Duplicate_list.Prog1_cov.duplicate_list_gen, "prog1_cov") ; (Duplicate_list.Prog2_cov.duplicate_list_gen, "prog2_cov") ; (Duplicate_list.Prog3_cov.duplicate_list_gen, "prog3_cov") ;
   (Duplicate_list.Prog1_safe.duplicate_list_gen, "prog1_safe") ; (Duplicate_list.Prog2_safe.duplicate_list_gen, "prog2_safe") ; (Duplicate_list.Prog3_safe.duplicate_list_gen, "prog3_safe") ]
 
 (* Unique lists *)
-let unique_list_generators = 
-  [ (Unique_list.Prog.unique_list_gen, "prog") ; 
-  (Unique_list.Prog1_syn.unique_list_gen, "prog1_syn") ; (Unique_list.Prog2_syn.unique_list_gen, "prog2_syn") ; (Unique_list.Prog3_syn.unique_list_gen, "prog3_syn") ; 
-  (Unique_list.Prog1_cov.unique_list_gen, "prog1_cov") ; (Unique_list.Prog2_cov.unique_list_gen, "prog2_cov") ; (Unique_list.Prog3_cov.unique_list_gen, "prog3_cov") ; 
+let unique_list_generators =
+  [ (Unique_list.Prog.unique_list_gen, "prog") ;
+  (Unique_list.Prog1_syn.unique_list_gen, "prog1_syn") ; (Unique_list.Prog2_syn.unique_list_gen, "prog2_syn") ; (Unique_list.Prog3_syn.unique_list_gen, "prog3_syn") ;
+  (Unique_list.Prog1_cov.unique_list_gen, "prog1_cov") ; (Unique_list.Prog2_cov.unique_list_gen, "prog2_cov") ; (Unique_list.Prog3_cov.unique_list_gen, "prog3_cov") ;
   (Unique_list.Prog1_safe.unique_list_gen, "prog1_safe") ; (Unique_list.Prog2_safe.unique_list_gen, "prog2_safe") ; (Unique_list.Prog3_safe.unique_list_gen, "prog3_safe") ;]
 
 (* sorted lists *)
